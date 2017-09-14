8e3c896587f54f2f0bfc574cec0dd34a9358c9fe
Andrew Kalenda <andrew.kalenda@gmail.com>
2017-09-13 18:57:19 -0700

    Added incidental tests for FacetedValuesJS

--- FacetedValuesJS/test/testFacetedValuesJS.js
+++ FacetedValuesJS/test/testFacetedValuesJS.js
@@ -1,12 +1,11 @@
 process.env.NODE_ENV = 'test';
-var pathToFVLib = '../src/FacetedValuesJS.js';
-var FacetedValuesJS = (require(pathToFVLib))(pathToFVLib);
+var FacetedValuesJS = require('../src/FacetedValuesJS.js');
 var fs = require('fs');
 
 exports.testFacetedValuesJS = {};
 [
     't00_basic'
-].forEach(function(testName){
+].forEach(function testBattery(testName){
     /**
      * @param {NodeUnit} test
      */
@@ -20,3 +19,32 @@ exports.testFacetedValuesJS = {};
         test.done();
     };
 });
+
+/**
+ * @param {NodeUnit} test
+ */
+exports.testFacetedValuesJS.testImport_Cloak = function testImport_Cloak(test){
+    test.expect(2);
+    var Cloak = FacetedValuesJS.Cloak;
+    var view = ['a'];
+    var x = Cloak(view, 11);
+    test.equal(x, 11);
+    view.pop();
+    test.notEqual(x, 11);
+    test.done();
+}
+
+/**
+ * @param {NodeUnit} test
+ */
+exports.testFacetedValuesJS.testImport_FacetedValue = function testImport_FacetedValue(test){
+    test.expect(1);
+    var FacetedValue = FacetedValuesJS.FacetedValue;
+    var expected = '<A ? <B ? 1 : 3> : <B ? 5 : 7>>';
+    var actual = new FacetedValue("A",
+        new FacetedValue("B", 1, 3),
+        new FacetedValue("B", 5, 7)
+    ).toString();
+    test.equal(actual, expected);
+    test.done();
+}
